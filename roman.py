2b728eb082c4bb9606c5a33e81739812be0aa81c
mrl5 <jakuboo+github@gmail.com>
2018-09-10 16:49:04 +0200

    "isinstance" condition should be checked first

--- roman.py
+++ roman.py
@@ -29,10 +29,11 @@ roman_numeral_map = (('M',  1000),
 
 def to_roman(n):
     '''convert integer to Roman numeral'''
-    if not (0 < n < 4000):
-        raise OutOfRangeError('number out of range (must be 1..3999)')
+    # modified by mrl5
     if not isinstance(n, int):
         raise NotIntegerError('non-integers can not be converted')
+    if not (0 < n < 4000):
+        raise OutOfRangeError('number out of range (must be 1..3999)')
     result = ''
     for numeral, integer in roman_numeral_map:
         while n >= integer:
